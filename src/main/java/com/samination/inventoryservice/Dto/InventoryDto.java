853cebfe8627cccf42fd47f11ffda3b6393244d7
samination <43864365+samination@users.noreply.github.com>
2022-11-28 12:21:53 +0100

    Implemented MicroServices

---
+++ src/main/java/com/samination/inventoryservice/Dto/InventoryDto.java
@@ -0,0 +1,14 @@
+package com.samination.inventoryservice.Dto;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InventoryDto {
+    private String skuCode;
+    private boolean isInStock;
+}
---
+++ src/main/java/com/samination/inventoryservice/Repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.samination.inventoryservice.Repository;
+
+import com.samination.inventoryservice.Model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
